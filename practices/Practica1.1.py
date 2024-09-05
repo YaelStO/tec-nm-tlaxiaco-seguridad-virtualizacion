a639783b4be1d0ebd50f2c49fa8400ed0e829587
YaelStO <125827570+YaelStO@users.noreply.github.com>
2024-09-05 14:26:32 -0600

    Practica.2

--- practices/Practica1.1.py
+++ practices/Practica1.1.py
@@ -5,7 +5,7 @@ def valida_contra(contra):
     if 8<= len(contra) <= 16: 
         if re.search('[a-z]', contra) and re.search('[A-Z]', contra):
             if re.search('[0-9]', contra):
-                if re.search('[$#@]', contra):
+                if re.search('[$#@%]', contra):
                     return True
                 return False
             
